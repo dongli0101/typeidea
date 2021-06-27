42b924b74590da85ca136e868f4f6018d7302dd8
dongle <1013768371@qq.com>
2021-06-27 20:35:41 +0800

    update init

--- typeidea_20210624/settings/base.py
+++ typeidea_20210624/settings/base.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'typeidea_20210624.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
     }
 }
 
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'Asian/Shanghai'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
